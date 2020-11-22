e07f85f866e67a419d61bd051bb90d92165a9c0d
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-11-22 18:37:38 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,12 @@
+for i in range(1,11):
+    for j in range(1,i+1):
+        print('*',end='')
+    print()
+n=40
+for k in range(1,11):
+    print(''*n,end='')
+    print('*'*(i))
+    n-=1
+x=int(input('enter num greater than 0'))
+assert x>0, "wrong value enetered"
+print('u entered',x)
