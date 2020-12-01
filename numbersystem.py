7393299ce99cbb7f70d33da609d7279a55f46a97
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-12-01 23:43:38 +0530

    Add files via upload

---
+++ numbersystem.py
@@ -0,0 +1,19 @@
+n=25
+print(bin(n))
+print(oct(n))
+print(hex(n))
+m=2.6+5.1j
+k=3-6.5j
+print("sum=",m+k)
+a=10
+b=-4
+print(complex(a,b))
+i=str(input('enter a hexadecimal num'))
+i=int(str,16)
+print('hexadecimal to decimal is=',i)
+j=str(input('enter an octal number'))
+j=int(str,8)
+print('octal to decimal is',j)
+k=str(input('enter a binary number'))
+k=int(str,2)
+print('binary to decimal is=',k)
